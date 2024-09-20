4131dc88b9f69b24f73e069f15bf19b3ca3a4d57
Joseemartinezz <148618793+Joseemartinezz@users.noreply.github.com>
2024-09-20 12:27:59 +0200

    Update shell_list.c

--- shell_list.c
+++ shell_list.c
@@ -1,9 +1,6 @@
 #include "shell_list.h"
 
-bool createNode(tPos *p){
-    *p = malloc(sizeof(struct tNode)); //allocates memory for a new node
-    return *p != NULL; //if memory allocation was successful return true, else return false
-}
+
 
 void createEmptyList (tList *L){
     *L = NULL; // sets the address pointed to by L to NULL
@@ -17,7 +14,41 @@ tPos first (tList L){
     return L;
 }
 
-//abcde
+tPosL last (tList L){
+    tPosL p;
+    for(p=L; p->next != LNULL; p=p->next);
+    return p;
+}
+
+tPosL next (tPosL p, tList L){
+    return p->next;
+}
+
+tPosL previous (tPosL p, tList L){
+    tPosL q;
+    if (p==L)
+        return LNULL;
+    else{
+        for (q=L; q->next != p ; q=q->next);
+        return q;
+    }
+}
+
+bool InsertItem(tItem d,tList L){
+    tPosS aux; 
+    if(!createNode(&aux)){
+        return false; 
+    } else{
+        aux->data=d; 
+        aux->next = L;
+        L =aux;
+        return true;
+    }
+}
+bool createNode(tPos *p){
+    *p = malloc(sizeof(struct tNode)); //allocates memory for a new node
+    return *p != NULL; //if memory allocation was successful return true, else return false
+}
 
 tPos findItem (tItem d, tList L){
     tPos p;
@@ -28,7 +59,21 @@ tPos findItem (tItem d, tList L){
         return NULL;
 }
 
-void deleteAtPositionU (tPos p, tList* L){}
+void deleteAtPosition (tPosL p, tList *L){
+    tPosL q;
+    if (p == *L) {
+        *L = (*L) -> next;
+    } else if (p->next == LNULL) {//delete last element
+        for (q = *L; q->next != p; q = q->next);//find position in front of p
+        q->next = LNULL;
+    } else {//delete from the middle
+        q = p->next;
+        p->data = q->data;
+        p->next = q->next;
+        p = q; //get rid of the node that used to be right after p
+    }
+    free(p);
+}
 
 void deleteList(tList L){
    tPos p = L.next;
