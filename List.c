2f094b9fba5f8ee968463942f44efa43694cbdb8
Pedro Saavedra Rubinos <pedrosaavedrarubinos@gmail.com>
2024-09-23 10:36:34 +0200

    New Functions

---
+++ List.c
@@ -0,0 +1,38 @@
+#include "stdlib.h"
+#include "string.h"
+#include "List.h"
+#include "stdio.h"
+#define MAX 2048
+
+int IsHistoricEmpty(HLIST *l){
+    return l->counter==0;
+    }
+
+void InitHistoric (HLIST *l){
+    l->counter=0;
+}
+
+int AddHistoricElement(HLIST *l, char * cmd){
+    if(l->counter == MAX-1){
+        errno=ENOSPC;//error number for not enough space
+        return -1;
+    }
+    if((l->command[l->counter]=strdup (cmd))==NULL)
+        return -1;
+    l->counter++;
+    return l->counter;
+}
+
+int ClearHistoric (HLIST *l){
+    int i;
+    for (i=0; i<l->counter;i++)
+        free(l->command[i]);
+}
+
+void PrintHistoric (HLIST *l){
+    int i;
+    for(i=0; i<l->counter;i++)
+        printf("%d->%s\n", i, l->command[i]);
+
+
+}
