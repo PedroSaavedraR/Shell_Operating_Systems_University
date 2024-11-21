c0938beb1b0f34a6d36b9fb966c79bb1bf54fba2
Pedro Saavedra <143135026+PedroSaavedraR@users.noreply.github.com>
2024-11-21 21:04:53 +0100

    Update files.c

--- files.c
+++ files.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 bool createNode(tPos *p){
     *p = malloc(sizeof(struct tNode)); //allocates memory for a new node
@@ -44,7 +45,7 @@ bool addfile(char *filename, int df, int mode, tfilelist *L) {
     if (*L == NULL) {
         *L = q;
     }
-    //Inserting at the end
+        //Inserting at the end
     else{
         p = *L;
         while(p->next != NULL && p->next->data.descriptor < df){
@@ -122,63 +123,116 @@ bool isemptymem (tmemlist m){
 }
 
 
-void printmemory(tmemlist m, char *method) {//bhb
+void printmemory(tmemlist m, char *method) {
     mPos p;
     if (strcmp(method, "all") == 0) {
-        for (p = m; p != NULL; p = p->next)
-            printf("%s %d %s %s %d %s\n", p->data.adress, p->data.size, p->data.date, p->data.method, p->data.kdf,
-                   p->data.filename);
+        printf("******Assigned blocks list for the process %d\n", getpid());
+        for (p = m; p != NULL; p = p->next) {
+            if (strcmp(p->data.method, "shared") == 0) {
+                // Special formatting for 'shared' method
+                printf("%p %4ld %s %s (key %d)\n", p->data.adress, p->data.size, p->data.date, p->data.method, p->data.kdf);
+            } else {
+                printf("%p %4ld %s %s\n", p->data.adress, p->data.size, p->data.date, p->data.method);
+            }
+
+            // Print additional fields if present
+            if (p->data.kdf != -1) {
+                printf("  %d\n", p->data.kdf);
+            }
+            if (*p->data.filename != '\0') {
+                printf("%s\n", p->data.filename);
+            }
+        }
     } else {
         for (p = m; p != NULL; p = p->next) {
-            if (strcmp(p->data.method, method) == 0)
-                printf("%s %d %s %s %d %s\n", p->data.adress, p->data.size, p->data.date, p->data.method, p->data.kdf,
-                       p->data.filename);
+            if (strcmp(p->data.method, method) == 0) {
+                // Print the memory block for the selected method
+                if (strcmp(p->data.method, "shared") == 0) {
+                    printf("%p %4ld %s %s (key %d)\n", p->data.adress, p->data.size, p->data.date, p->data.method, p->data.kdf);
+                } else {
+                    printf("%p %4ld %s %s\n", p->data.adress, p->data.size, p->data.date, p->data.method);
+                }
+                if (p->data.kdf != -1) {
+                    // Print the KDF value if it's not -1
+                    printf("  %d\n", p->data.kdf);
+                }
+                if (*p->data.filename != '\0') {
+                    // Print filename if it's not empty
+                    printf("%s\n", p->data.filename);
+                }
+            }
         }
     }
 }
 
-bool addmem(char *address, int size, char *date, char *method, char *filename, int kdf, tmemlist *m) {
+bool addmem(void *address, long size,char* date,char *method, char *filename, int kdf, tmemlist *m) {
     mPos q, p;
+    if(address == NULL)
+        return false;
 
-    if (!createNodem(&q)) {
+    if (!createNodem(&q) || q == NULL) {
+        fprintf(stderr, "Error: Could not create node\n");
         return false;
     }
-    snprintf(q->data.adress, MAX, "%p", (void *)address);
-    strcpy(q->data.method, method);
-    strncpy(q->data.date, date, MAX - 1);
-    strncpy(q->data.filename, filename, MAX- 1);
+
+    if (method != NULL) {
+        strncpy(q->data.method,method,MAX-1);
+        q->data.method[MAX - 1] = '\0';
+    } else {
+        q->data.method[0] = '\0';
+    }
+
+    if (date != NULL) {
+        strncpy(q->data.date, date, MAX - 1);
+        q->data.date[MAX - 1] = '\0';
+    } else {
+        q->data.date[0] = '\0';
+    }
+
+    if (filename != NULL) {
+        strncpy(q->data.filename, filename, MAX - 1);
+        q->data.filename[MAX - 1] = '\0';
+    } else {
+        q->data.filename[0] = '\0';
+    }
+
+    q->data.adress = address;
     q->data.size = size;
-    q->data.kdf = kdf;
+    q->data.kdf = kdf == 0 ? -1 : kdf;
     q->next = NULL;
 
-    // If the list is empty, set this node as the head
     if (*m == NULL) {
         *m = q;
         return true;
     }
-
-    // Traverse to the end of the list
     p = *m;
     while (p->next != NULL) {
         p = p->next;
     }
 
-    // Append the new node
     p->next = q;
 
     return true;
 }
 
-
-mPos findmemad(char *adress,tmemlist *m) {
+mPos findmemad(void* adress,tmemlist *m) {
     mPos p = *m;
-    while(p != NULL && p->data.adress != adress);
+    while(p != NULL && p->data.adress != adress){
+        p = p->next;}
     return p;
 }
 
 mPos findmemsz(int size,tmemlist *m){
     mPos p = *m;
-    while(p != NULL && p->data.size != size);
+    while(p != NULL && p->data.size != (long )size)
+        p = p->next;
+    return p;
+}
+
+mPos findmemsh(int key,tmemlist *m){
+    mPos p = *m;
+    while(p != NULL && p->data.kdf != key)
+        p = p->next;
     return p;
 }
 
