3356f6a69bb7310b2e62245ea4011224dd2560ca
Pedro Saavedra <143135026+PedroSaavedraR@users.noreply.github.com>
2024-12-02 10:27:11 +0100

    Update files.h

--- files.h
+++ files.h
@@ -7,6 +7,8 @@
 #define MAX 2048
 #include <stdbool.h>
 
+enum status {FINISHED , STOPPED , SIGNALED, ACTIVE};
+
 typedef struct file{
     char filename[MAX];
     int mode;
@@ -22,8 +24,19 @@ typedef struct memory{
     char filename[MAX];
 }tmemory;
 
+typedef struct process{
+    int PID;
+    char date[MAX];
+    enum status state;
+    char comline[MAX];
+    int priority;
+}tprocess;
+
+
+
 typedef struct tNode* tPos;
 typedef struct mNode* mPos;
+typedef struct pNode* pPos;
 
 struct tNode{
     tfile data;
@@ -35,8 +48,14 @@ struct mNode{
     mPos next;
 };
 
+struct pNode{
+    tprocess data;
+    pPos next;
+};
+
 typedef tPos tfilelist;
 typedef mPos tmemlist;
+typedef pPos tproclist;
 
 bool initfilelist(tfilelist* list);
 bool isemptyfiles(tfilelist);
@@ -61,7 +80,14 @@ bool addmem(void *address,long size,char* date,char *method,char *filename,int k
 mPos findmemad(void* adress,tmemlist*);
 mPos findmemsz(int size,tmemlist*);
 mPos findmemsh(int key,tmemlist*);
+mPos findmemfl(char* filename,tmemlist*);
 void closemem(mPos,tmemlist*);
 void FreeMemList(tmemlist);
+
+void createproclist(tproclist*);
+bool isemptyproc(tproclist);
+bool addproc(tprocess,tproclist*);
+pPos findproc(int pid, tproclist*);
+void freeproclist(tproclist);
 
 #endif //S0_P0_FILES_H
